0b57b7eeeb9823acfed36814aae167016750810e
shandiru <msddiru@gmail.com>
2026-02-03 23:00:19 +0530

    added

--- frontend/src/components/ProductSection.jsx
+++ frontend/src/components/ProductSection.jsx
@@ -9,7 +9,7 @@ export default function ProductSellers() {
   const [loading, setLoading] = useState(true);
 
   // Directly using localhost as requested
-  const BASE_URL = "http://localhost:4000";
+  const BASE_URL = "https://ma-auto-electricals.onrender.com/";
 
   useEffect(() => {
     const fetchProducts = async () => {
