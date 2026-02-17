d0329489d1732a4fc75ee8660afc29775fd479d2
shandiru <msddiru@gmail.com>
2026-02-17 19:06:22 +0530

    added

--- ma-admin/src/App.jsx
+++ ma-admin/src/App.jsx
@@ -25,7 +25,7 @@ import Signup from "./components/Signup";
 import InvoiceGenerator from "./pages/InvoicePage";
 import InvoiceDataPage from "./pages/InvoiceDataPage";
 
-const url = "http://localhost:4000"; // Backend URL
+const url = "https://ma-auto-electricals.onrender.com"; // Backend URL
 
 /* ---------------- AUTH ROUTE ---------------- */
 const AuthRoute = ({ children }) => {
